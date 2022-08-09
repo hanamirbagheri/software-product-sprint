4c67df85ed265936405c0cb2c578b3d4f27a8607
hana <hana.mirbagheri1@gmail.com>
2022-08-09 17:50:22 +0000

    WEEK2

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,6 +15,7 @@
 /**
  * Adds a random greeting to the page.
  */
+
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
@@ -25,4 +26,12 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+async function get_response(){
+    const response = await fetch('/hello');
+    const responsetotext = await response.text();
+
+    const placeholder = document.getElementById('response-acquired');
+    placeholder.innerText = responsetotext;
 }
