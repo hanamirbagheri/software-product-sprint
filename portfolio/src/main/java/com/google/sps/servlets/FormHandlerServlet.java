222421ac10412d3422c296ca6877a7a6335478b2
hana <hana.mirbagheri1@gmail.com>
2022-08-12 15:46:01 +0000

    Datastore of conatct elements

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -5,23 +5,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String nameValue = Jsoup.clean(request.getParameter("name-input"), Safelist.none());
+    String numberValue = Jsoup.clean(request.getParameter("number-input"), Safelist.none());
+    String emailValue = Jsoup.clean(request.getParameter("email-input"), Safelist.none());
+    long timestamp = System.currentTimeMillis();
 
-    // Get the value entered in the form.
-    String nameValue = request.getParameter("name-input");
-    String numberValue = request.getParameter("number-input");
-    String emailValue = request.getParameter("email-input");
-
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + nameValue + numberValue + emailValue);
-
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + nameValue + numberValue + emailValue);
-    response.sendRedirect("http://hmirbagheri-sps-summer22.appspot.com/");
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Contact");
+    FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("name", nameValue)
+            .set("number", numberValue)
+            .set("email", emailValue)
+            .set("timestamp", timestamp)
+            .build();
+    datastore.put(taskEntity);
+    response.sendRedirect("/index.html");
+    
   }
 }
